57dde050d362243307b48325189a2e4fb29c9e02
Артем Погребной <tem.rdm@gmail.com>
2015-04-29 22:34:57 +0700

    Add dist ENV, check dist package exist

--- 9.2-1С/patch-postgresql-contrib.sh
+++ 9.2-1С/patch-postgresql-contrib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-dpkg -x /opt/postgresql-contrib-${PG_MAJOR}_${PG_VERSION}.1C_${ARCH}.deb /opt/tmpdir/
-dpkg -e /opt/postgresql-contrib-${PG_MAJOR}_${PG_VERSION}.1C_${ARCH}.deb /opt/tmpdir/DEBIAN
+dpkg -x /opt/postgresql-contrib-${PG_MAJOR}_${PG_VERSION}.1C_${PG_ARCH}.deb /opt/tmpdir/
+dpkg -e /opt/postgresql-contrib-${PG_MAJOR}_${PG_VERSION}.1C_${PG_ARCH}.deb /opt/tmpdir/DEBIAN
 sed -i 's/libicu46/libicu52/g' /opt/tmpdir/DEBIAN/control
-dpkg -b /opt/tmpdir /opt/postgresql-contrib-${PG_MAJOR}_${PG_VERSION}.1C_${ARCH}_patched.deb
+dpkg -b /opt/tmpdir /opt/postgresql-contrib-${PG_MAJOR}_${PG_VERSION}.1C_${PG_ARCH}_patched.deb
 rm -rf /opt/tmpdir/
